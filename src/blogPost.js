3bf34cd8253b82cdcc825e60b3d0df6410ba4a87
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-11 20:46:11 -0400

    furthered the association

--- src/blogPost.js
+++ src/blogPost.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
   title: String,
   content: String,
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  comments: [{ type: Schema.Types.ObjectId, ref: "comment" }],
 });
 
 const BlogPost = mongoose.model("blogPost", BlogPostSchema);
